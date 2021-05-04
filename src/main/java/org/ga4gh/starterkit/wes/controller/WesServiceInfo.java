2e77f77d721b6e560466768f733955aa2b826f4e
Jeremy Adams <jeremy.adams@ga4gh.org>
2021-05-04 14:56:06 -0400

    renamed service info controller to WesServiceInfo, avoid bean name collision with other GA4GH APIs

--- src/main/java/org/ga4gh/starterkit/wes/controller/WesServiceInfo.java
+++ src/main/java/org/ga4gh/starterkit/wes/controller/WesServiceInfo.java
@@ -4,21 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import static org.ga4gh.starterkit.wes.constant.WesApiConstants.WES_API_V1;
-
-import org.ga4gh.starterkit.wes.model.WesServiceInfo;
 import org.springframework.http.MediaType;
 
 @RestController
 @RequestMapping(WES_API_V1 + "/service-info")
-public class ServiceInfo {
+public class WesServiceInfo {
 
     @Autowired
-    WesServiceInfo wesServiceInfo;
+    org.ga4gh.starterkit.wes.model.WesServiceInfo wesServiceInfo;
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public WesServiceInfo getServiceInfo() {
+    public org.ga4gh.starterkit.wes.model.WesServiceInfo getServiceInfo() {
         return wesServiceInfo;
     }
 }
