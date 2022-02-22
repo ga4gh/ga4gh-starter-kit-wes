ba56e7dd14b2f93b77182da23e91a93070a4c44c
Emre Ferhatoglu <97183433+emre-f@users.noreply.github.com>
2022-02-22 14:28:36 -0500

    test

--- src/main/java/org/ga4gh/starterkit/wes/utils/requesthandler/GetRunLogRequestHandler.java
+++ src/main/java/org/ga4gh/starterkit/wes/utils/requesthandler/GetRunLogRequestHandler.java
@@ -65,6 +65,12 @@ public class GetRunLogRequestHandler implements RequestHandler<RunLog> {
         // methods to obtain run status
         try {
             RunManager runManager = runManagerFactory.createRunManager(wesRun);
+            System.out.print("****************************** \n");
+            System.out.print(runManager.getLanguageHandler().toString() + "\n");
+            System.out.print(runManager.getLanguageHandler().getLanguageConfig().toString() + "\n");
+            // System.out.print(runManager.getEngineConfig() + "\n");
+            System.out.print(runManager.getEngineHandler() + "\n");
+            System.out.print("****************************** \n");
             LanguageHandler runTypeDetailsHandler = runManager.getLanguageHandler();
             System.out.print("- RUN TYPE DETAILS HANDLER: - \n");
             System.out.print(runTypeDetailsHandler + "\n");
