1217eac698778fdfe845401b8a25ce3a97a90feb
Emre Ferhatoglu <97183433+emre-f@users.noreply.github.com>
2022-03-07 14:25:51 -0500

    lets see it

--- src/test/java/integration/DemoNextFlowTest.java
+++ src/test/java/integration/DemoNextFlowTest.java
@@ -166,77 +166,77 @@ public class DemoNextFlowTest extends AbstractTestNGSpringContextTests
                 }}
             },
             // WORKFLOW RUN 1: CUSTOM WES
-            {
-                CUSTOM_PUBLIC_URL,
-                "{\"message_1\":\"HELLOWORLD\",\"message_2\":\"FOO\",\"message_3\":\"BAR\"}",
-                new ExpectedLogValues() {{
-                    setExpName("jb-adams/echo-nf");
-                    setExpCmd(new ArrayList<String>(Arrays.asList(
-                        "#!/bin/bash -ue",
-                        "echo \"STDOUT: writing message 1 to message_1.txt\"",
-                        "echo \"STDERR: writing message 1 to message_1.txt\" >&2",
-                        "echo \"Current message: HELLOWORLD\" > message_1.txt",
-                        "#!/bin/bash -ue",
-                        "echo \"STDOUT: writing message 2 to message_2.txt\"",
-                        "echo \"STDERR: writing message 2 to message_2.txt\" >&2",
-                        "echo \"Contents of previous message: `cat message_1.txt`\"",
-                        "echo \"Current message: FOO\" > message_2.txt",
-                        "#!/bin/bash -ue",
-                        "echo \"STDOUT: writing message 3 to message_3.txt\"",
-                        "echo \"STDERR: writing message 3 to message_3.txt\" >&2",
-                        "echo \"Contents of previous message: `cat message_2.txt`\"",
-                        "echo \"Current message: BAR\" > message_3.txt"
-                    )));
-                    setExpStdoutMd5("a8aaf562ecccb0b877db257d8dff66e4");
-                    setExpStderrMd5("5f19d60316bff6cdb924a419e6d2ca39");
-                    setExpExitCode(0);
-                }},
-                new ArrayList<ExpectedLogValues>() {{
-                    add(new ExpectedLogValues() {{
-                        setExpName("output_message_1");
-                        setExpCmd(new ArrayList<String>(Arrays.asList(
-                            "#!/bin/bash -ue",
-                            "echo \"STDOUT: writing message 1 to message_1.txt\"",
-                            "echo \"STDERR: writing message 1 to message_1.txt\" >&2",
-                            "echo \"Current message: HELLOWORLD\" > message_1.txt"
-                        )));
-                        setExpStdoutMd5("4dd961d06916fd0ca9b9db4f41553693");
-                        setExpStderrMd5("2785ec9fa04371e914be4a64b307910c");
-                        setExpExitCode(0);
-                    }});
-                    add(new ExpectedLogValues() {{
-                        setExpName("output_message_2");
-                        setExpCmd(new ArrayList<String>(Arrays.asList(
-                            "#!/bin/bash -ue",
-                            "echo \"STDOUT: writing message 2 to message_2.txt\"",
-                            "echo \"STDERR: writing message 2 to message_2.txt\" >&2",
-                            "echo \"Contents of previous message: `cat message_1.txt`\"",
-                            "echo \"Current message: FOO\" > message_2.txt"
-                        )));
-                        setExpStdoutMd5("46bb429850e032880977337362994bd1");
-                        setExpStderrMd5("260c7e55c4a5e4c0f3ef4014acdad12d");
-                        setExpExitCode(0);
-                    }});
-                    add(new ExpectedLogValues() {{
-                        setExpName("output_message_3");
-                        setExpCmd(new ArrayList<String>(Arrays.asList(
-                            "#!/bin/bash -ue",
-                            "echo \"STDOUT: writing message 3 to message_3.txt\"",
-                            "echo \"STDERR: writing message 3 to message_3.txt\" >&2",
-                            "echo \"Contents of previous message: `cat message_2.txt`\"",
-                            "echo \"Current message: BAR\" > message_3.txt"
-                        )));
-                        setExpStdoutMd5("8cd93c14c6a2bb5142ce6c3769274311");
-                        setExpStderrMd5("94e537de7cf7343e7a834bc8de52d10b");
-                        setExpExitCode(0);
-                    }});
-                }},
-                new HashMap<String, String>() {{
-                    put("message_1.txt", "9ebb038f0cf3b8c6e0d98b0e7a723aff");
-                    put("message_2.txt", "9b9384b69c2f2c2cf653f25b6725d59a");
-                    put("message_3.txt", "4fb1af51dbd9aa628a1b34d9e08e6d1b");
-                }}
-            }
+            // {
+            //     CUSTOM_PUBLIC_URL,
+            //     "{\"message_1\":\"HELLOWORLD\",\"message_2\":\"FOO\",\"message_3\":\"BAR\"}",
+            //     new ExpectedLogValues() {{
+            //         setExpName("jb-adams/echo-nf");
+            //         setExpCmd(new ArrayList<String>(Arrays.asList(
+            //             "#!/bin/bash -ue",
+            //             "echo \"STDOUT: writing message 1 to message_1.txt\"",
+            //             "echo \"STDERR: writing message 1 to message_1.txt\" >&2",
+            //             "echo \"Current message: HELLOWORLD\" > message_1.txt",
+            //             "#!/bin/bash -ue",
+            //             "echo \"STDOUT: writing message 2 to message_2.txt\"",
+            //             "echo \"STDERR: writing message 2 to message_2.txt\" >&2",
+            //             "echo \"Contents of previous message: `cat message_1.txt`\"",
+            //             "echo \"Current message: FOO\" > message_2.txt",
+            //             "#!/bin/bash -ue",
+            //             "echo \"STDOUT: writing message 3 to message_3.txt\"",
+            //             "echo \"STDERR: writing message 3 to message_3.txt\" >&2",
+            //             "echo \"Contents of previous message: `cat message_2.txt`\"",
+            //             "echo \"Current message: BAR\" > message_3.txt"
+            //         )));
+            //         setExpStdoutMd5("a8aaf562ecccb0b877db257d8dff66e4");
+            //         setExpStderrMd5("5f19d60316bff6cdb924a419e6d2ca39");
+            //         setExpExitCode(0);
+            //     }},
+            //     new ArrayList<ExpectedLogValues>() {{
+            //         add(new ExpectedLogValues() {{
+            //             setExpName("output_message_1");
+            //             setExpCmd(new ArrayList<String>(Arrays.asList(
+            //                 "#!/bin/bash -ue",
+            //                 "echo \"STDOUT: writing message 1 to message_1.txt\"",
+            //                 "echo \"STDERR: writing message 1 to message_1.txt\" >&2",
+            //                 "echo \"Current message: HELLOWORLD\" > message_1.txt"
+            //             )));
+            //             setExpStdoutMd5("4dd961d06916fd0ca9b9db4f41553693");
+            //             setExpStderrMd5("2785ec9fa04371e914be4a64b307910c");
+            //             setExpExitCode(0);
+            //         }});
+            //         add(new ExpectedLogValues() {{
+            //             setExpName("output_message_2");
+            //             setExpCmd(new ArrayList<String>(Arrays.asList(
+            //                 "#!/bin/bash -ue",
+            //                 "echo \"STDOUT: writing message 2 to message_2.txt\"",
+            //                 "echo \"STDERR: writing message 2 to message_2.txt\" >&2",
+            //                 "echo \"Contents of previous message: `cat message_1.txt`\"",
+            //                 "echo \"Current message: FOO\" > message_2.txt"
+            //             )));
+            //             setExpStdoutMd5("46bb429850e032880977337362994bd1");
+            //             setExpStderrMd5("260c7e55c4a5e4c0f3ef4014acdad12d");
+            //             setExpExitCode(0);
+            //         }});
+            //         add(new ExpectedLogValues() {{
+            //             setExpName("output_message_3");
+            //             setExpCmd(new ArrayList<String>(Arrays.asList(
+            //                 "#!/bin/bash -ue",
+            //                 "echo \"STDOUT: writing message 3 to message_3.txt\"",
+            //                 "echo \"STDERR: writing message 3 to message_3.txt\" >&2",
+            //                 "echo \"Contents of previous message: `cat message_2.txt`\"",
+            //                 "echo \"Current message: BAR\" > message_3.txt"
+            //             )));
+            //             setExpStdoutMd5("8cd93c14c6a2bb5142ce6c3769274311");
+            //             setExpStderrMd5("94e537de7cf7343e7a834bc8de52d10b");
+            //             setExpExitCode(0);
+            //         }});
+            //     }},
+            //     new HashMap<String, String>() {{
+            //         put("message_1.txt", "9ebb038f0cf3b8c6e0d98b0e7a723aff");
+            //         put("message_2.txt", "9b9384b69c2f2c2cf653f25b6725d59a");
+            //         put("message_3.txt", "4fb1af51dbd9aa628a1b34d9e08e6d1b");
+            //     }}
+            // }
         };
     }
 
