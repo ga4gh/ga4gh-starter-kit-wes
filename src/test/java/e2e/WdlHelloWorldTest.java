d3337309cd9468d9c2ac34c0650d2c64b5646096
Jeremy Adams <jeremy.adams@ga4gh.org>
2022-03-15 09:01:34 -0400

    attempt to restore service info test under docker testing

--- src/test/java/e2e/WdlHelloWorldTest.java
+++ src/test/java/e2e/WdlHelloWorldTest.java
@@ -1,8 +1,9 @@
 package e2e;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-
+import java.util.Arrays;
 import org.ga4gh.starterkit.wes.model.WorkflowType;
 import org.ga4gh.starterkit.wes.testutils.ExpectedLogValues;
 import org.ga4gh.starterkit.wes.testutils.WesE2ERunAndMonitorWorkflow;
@@ -16,17 +17,36 @@ public class WdlHelloWorldTest extends WesE2ERunAndMonitorWorkflow {
     private static final String WORKFLOW_PARAMS = "{}";
     
     private static final ExpectedLogValues EXP_RUN_LOG = new ExpectedLogValues() {{
-        
+        setExpName("helloWorld");
+        setExpCmd(new ArrayList<String>(Arrays.asList(
+            "echo \"hello world\"",
+            "echo \"world\"",
+            "echo \"hello\""
+        )));
+        setExpStdoutMd5(null);
+        setExpStderrMd5(null);
+        setExpExitCode(0);
     }};
 
     private static final List<ExpectedLogValues> EXP_TASK_LOGS = new ArrayList<>() {{
-        add(new ExpectedLogValues());
+        add(new ExpectedLogValues() {{
+            // setExpName(expName);
+        }});
+        add(new ExpectedLogValues() {{
+            
+        }});
+        add(new ExpectedLogValues() {{
+            
+        }});
+    }};
+
+    private static final HashMap<String, String> EXP_OUTPUT_MD5 = new HashMap<>() {{
+        put(null, null);
     }};
 
 
     @Test
-    public void test() {
-        // runEndToEndTest(WORKFLOW_TYPE, WORKFLOW_TYPE_VERSION, WORKFLOW_URL, WORKFLOW_PARAMS, expRunLog, expTaskLogs, expOutputMd5Map);
+    public void test() throws Exception {
+        // runEndToEndTest(WORKFLOW_TYPE, WORKFLOW_TYPE_VERSION, WORKFLOW_URL, WORKFLOW_PARAMS, EXP_RUN_LOG, EXP_TASK_LOGS, EXP_OUTPUT_MD5);
     }
-    
 }
